539f2ba986f82a67f11c8e76341845acc3b95b07
mkmsyk <mkmsy@Omen>
2026-06-29 18:24:37 +0900

    fix: deploy.ps1のパス取得に (Get-Location).Path を使用するように変更

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,7 @@
 $ErrorActionPreference = 'Stop'
 
 # ディレクトリパスの設定
-$scriptDir = $PSScriptRoot
+$scriptDir = (Get-Location).Path
 $monoralDir = "C:\Users\mkmsy\Repositories\MONORAL"
 $cmsDir = "C:\Users\mkmsy\Repositories\CMS"
 $cmsGameTarget = Join-Path $cmsDir "src\main\resources\public\games\gb-demo"
