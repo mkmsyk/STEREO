baaa1935d913e5e869584ad9da001dc8a9272a8d
mkmsyk <mkmsy@Omen>
2026-06-29 18:24:53 +0900

    fix: deploy.ps1のパス定義を完全ローカル絶対パス直書きに固定

--- deploy.ps1
+++ deploy.ps1
@@ -4,7 +4,7 @@
 $ErrorActionPreference = 'Stop'
 
 # ディレクトリパスの設定
-$scriptDir = (Get-Location).Path
+$scriptDir = "C:\Users\mkmsy\Repositories\STEREO"
 $monoralDir = "C:\Users\mkmsy\Repositories\MONORAL"
 $cmsDir = "C:\Users\mkmsy\Repositories\CMS"
 $cmsGameTarget = Join-Path $cmsDir "src\main\resources\public\games\gb-demo"
